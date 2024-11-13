a3a8679d584277fe64937c46aad2ac6d6300773f
EtienneBrunet <51758248+EtienneBrunet@users.noreply.github.com>
2024-11-13 13:55:05 +0100

    Update beforeAndAfter.cy.js

--- beforeAndAfter.cy.js
+++ beforeAndAfter.cy.js
@@ -1,30 +1,34 @@
 describe('Campagne de test before and after', () => {
     // Est exécuté dès qu'on rentre dans describe()
     before(() => {
-        console.log('Début des tests')
+        cy.log('Début des tests')
     })
 
     // Est exécuté avant chaque test > it()
     beforeEach(() => {
-        console.log('Démarrage du test')
+        cy.log('Démarrage du test')
     })
 
     // Est exécuté quand tous les tests sont finis
     after(() => {
-        console.log('La campagne de test est finie')
+        cy.log('La campagne de test est finie')
     })
 
     // Est exécuté après chaque test > it()
     afterEach(() => {
-        console.log('Fin du test')
+        cy.log('Fin du test')
     })
 
     it('TEST 1 > OK', () => {
         expect(true).to.equal(true);
     })
 
-    it('TEST 2 > KO', () => {
+    it.skip('TEST 2 > KO', () => {
         expect(true).to.equal(false);
+    })
+    
+    it('TEST 3 > OK', () => {
+        expect(true).to.equal(true);
     })
 })
 
